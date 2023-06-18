225625d445563190e6e0c832351197a37b3b4ec3
mc-cari <mauriciocari2@gmail.com>
2023-06-18 18:31:06 -0400

    fix(pocketbase): remove pocketbase

--- app/mapLibre/page.tsx
+++ app/mapLibre/page.tsx
@@ -1,45 +1,10 @@
 import MapLibre from "./mapLibre";
 import PlacesJSON from '../places.json';
 
-function convertToGeoJSONPoint(place : any) {
-  const { id, name, category, campus, floor, parentId, latitude, longitude, description } = place;
-  return {
-    type: 'Feature',
-    properties: {
-      id,
-      name,
-      category,
-      description,
-      campus,
-      floor,
-      parentId,
-    },
-    geometry: {
-      type: 'Point',
-      coordinates: [longitude, latitude],
-    },
-  };
-}
-
-async function getPlacesInGeoJSON() {
-  
-  const response = await fetch('http://127.0.0.1:8090/api/collections/places/records?page=1&perPage=30', { cache: 'no-store' });
-  const data = await response.json();
-  const geoJSONFeatures = data?.items.map(convertToGeoJSONPoint);
-
-  const geoJSONPlaces = {
-    type: 'FeatureCollection',
-    features: geoJSONFeatures,
-  };
-  
-  return geoJSONPlaces;
-}
 
 export default async function Page() {
 
-  const Places = await getPlacesInGeoJSON();
- 
   return (
-    <MapLibre Places={Places}/>
+    <MapLibre Places={PlacesJSON}/>
   )
 }
