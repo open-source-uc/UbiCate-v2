2688a3f94927f6dfce2777374960239f172e0767
mc-cari <mauriciocari2@gmail.com>
2023-07-24 00:01:27 -0400

    feat(form-geo): update style

--- app/form-geo/NumberInputDisabled.tsx
+++ app/form-geo/NumberInputDisabled.tsx
@@ -1,29 +1,25 @@
-"use client";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 interface formikField {
   name: string;
+  value: string;
   onChange: any;
   onBlur: any;
 }
 
-export default function NumberInputDisabled({
-  label,
-  id,
-  value,
-  field,
-}: {
-  label: string;
-  id: string;
-  value: number;
-  field: formikField;
-}) {
+export default function NumberInputDisabled({ label, id, field }: { label: string; id: string; field: formikField }) {
   return (
-    <div className="flex flex-col w-1/2 px-4 justify-center items-center">
+    <div className="flex flex-col w-1/2 px-4 justify-center items-center text-light-4">
       <label className="my-2" htmlFor={id}>
         {label}
       </label>
-      <input id={id} disabled className="my-2 w-1/2 text-center pl-3" type="number" {...field} />
+      <input
+        id={id}
+        disabled
+        className="mu-2 block p-2.5 w-full text-s rounded-lg border bg-dark-3 border-dark-4 text-light-4 focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        type="number"
+        {...field}
+      />
     </div>
   );
 }
--- app/form-geo/page.tsx
+++ app/form-geo/page.tsx
@@ -10,6 +10,14 @@ interface newPlace {
   longitude: number;
   latitude: number;
   placeName: string;
+  information: string;
+}
+
+interface errors {
+  longitude?: string;
+  latitude?: string;
+  placeName?: string;
+  information?: string;
 }
 
 export default function Page() {
@@ -18,9 +26,7 @@ export default function Page() {
   const [latitude, setLatitude] = useState<number>((-33.5021 + -33.4952) / 2);
 
   const validate = (newPlace: newPlace) => {
-    console.log(newPlace, newPlace.longitude, !newPlace.longitude);
-
-    const boundaries = [
+    const campusBoundaries = [
       { campus: "San Joaquin", longitudeRange: [-70.6171, -70.6043], latitudeRange: [-33.5021, -33.4952] },
       { campus: "Lo Contador", longitudeRange: [-70.6198, -70.6154], latitudeRange: [-33.4207, -33.4178] },
       { campus: "Villarrica", longitudeRange: [-72.2264, -72.2244], latitudeRange: [-39.2787, -39.2771] },
@@ -28,16 +34,16 @@ export default function Page() {
       { campus: "Oriente", longitudeRange: [-70.597, -70.5902], latitudeRange: [-33.4477, -33.4435] },
     ];
 
-    const errors = {};
+    const errors: errors = {};
 
     if (newPlace.longitude === null) {
-      errors.longitude = "Required";
+      errors.longitude = "Requerido";
     } else if (newPlace.latitude === null) {
-      errors.latitude = "Required";
+      errors.latitude = "Requerido";
     } else {
-      var campus: string = null;
+      var campus: string | null = null;
 
-      for (const boundary of boundaries) {
+      for (const boundary of campusBoundaries) {
         if (
           newPlace.longitude >= boundary.longitudeRange[0] &&
           newPlace.longitude <= boundary.longitudeRange[1] &&
@@ -49,13 +55,17 @@ export default function Page() {
         }
       }
 
-      if (!campus) errors.longitude = "No campus found for the given coordinates";
+      if (!campus) errors.latitude = "Ubicacion fuera de los campus";
     }
 
     if (!newPlace.placeName) {
-      errors.placeName = "Required";
+      errors.placeName = "Requerido";
     } else if (newPlace.placeName.length > 40) {
-      errors.placeName = "Must be 15 characters or less";
+      errors.placeName = "Nombre demasiado largo";
+    }
+
+    if (newPlace.information && newPlace.information.length > 200) {
+      errors.information = "Informacion demasiado larga";
     }
 
     return errors;
@@ -70,47 +80,60 @@ export default function Page() {
   }
 
   useEffect(() => {
-    // timeout
+    // setear ubicacion actual con geolocalizacion
     const timeout = setTimeout(() => {
       setLatitude(1);
       setLongitude(1);
       console.log("timeout");
-    }, 2000);
+    }, 8000);
   }, []);
 
   return (
-    <div className="flex h-full w-full items-center justify-center">
-      <div className="flex flex-col w-1/2 h-2/3 bg-green-300 items-center justify-center rounded">
+    <div className="flex h-full w-full items-center justify-center bg-dark-1">
+      <div className="flex flex-col w-3/4 h-3/4 items-center justify-center rounded bg-dark-2">
         <Formik
-          initialValues={{ longitude: longitude, latitude: latitude, placeName: "" }}
+          initialValues={{ longitude: longitude, latitude: latitude, placeName: "", information: "" }}
           onSubmit={handleSubmit}
           validate={validate}
+          enableReinitialize={true}
         >
-          {({ isSubmitting }) => (
+          {() => (
             <Form className="flex flex-col justify-center items-center">
               <div className="flex flex-row w-full">
-                <Field
-                  id="longitude"
-                  name="longitude"
-                  label="Longitud"
-                  component={NumberInputDisabled}
-                />
-                <Field id="latitude" name="latitude" label="Latitud" value={13} component={NumberInputDisabled} type='number' />
+                <Field id="longitude" name="longitude" label="Longitud" component={NumberInputDisabled} />
+                <Field id="latitude" name="latitude" label="Latitud" component={NumberInputDisabled} />
               </div>
 
-              <ErrorMessage name="longitude" />
-              <ErrorMessage name="latitude" />
+              <ErrorMessage className="text-error text-sm w-3/4 text-center" name="longitude" component="div" />
+              <ErrorMessage className="text-error text-sm w-3/4 text-center" name="latitude" component="div" />
 
-              <label className="my-2 flex items-center justify-center" htmlFor="placeName">
-                Nombre del lugar a agregar
+              <label className="my-2 flex items-center justify-center text-light-4" htmlFor="placeName">
+                Sala
               </label>
-              <Field className="my-2 w-1/2" name="placeName" id="placeName" type="text" />
-              <ErrorMessage name="placeName" />
+              <Field
+                className="mu-2 block p-2.5 w-full text-s rounded-lg border bg-dark-3 border-dark-4 text-light-4 focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                name="placeName"
+                id="placeName"
+                type="text"
+              />
+              <ErrorMessage className="text-error text-sm w-full text-left" name="placeName" component="div" />
+
+              <label className="my-2 flex items-center justify-center text-light-4" htmlFor="information">
+                Informacion (opcional)
+              </label>
+              <Field
+                className="block p-2.5 w-full text-s rounded-lg border bg-dark-3 border-dark-4 text-light-4 focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                name="information"
+                id="information"
+                type="text"
+                component="textarea"
+              />
+              <ErrorMessage className="text-error text-sm w-full text-left" name="information" component="div" />
 
               <button
-                className="my-2 bg-lime-500 enabled:hover:bg-lime-400 text-white font-bold py-2 px-4 border-b-4 border-lime-700 enabled:hover:border-lime-500 rounded disabled:opacity-50 disabled:cursor-not-allowed"
+                className="my-2 text-light-4 bg-dark-3 enabled:hover:bg-dark-4 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center disabled:opacity-50 disabled:cursor-not-allowed"
                 type="submit"
-                disabled={isSubmitting}
+                disabled={submitting}
               >
                 Submit
               </button>
