6f8ca06e641b394e6c93719b0bfa7c6b61c01bc6
diegocostares <diegocostar@uc.cl>
2023-09-04 23:51:25 -0300

    feat(next.config): redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  async redirects() {
+    // TODO: MODIFICAR EN EL FUTURO
+	// El codigo se dejo comentado ya que no compila al hacer run build
+    //   if (process.env.NODE_ENV === "production") {
+    console.log("Ejecutando rewrites para producción");
+    return [
+      {
+        source: "/",
+        destination: "/form-geo",
+		permanent: true,
+      },
+      {
+        source: "/map",
+        destination: "/form-geo",
+		permanent: true,
+      },
+    ];
+    //   } else {
+    // 	console.log("Ejecutando en un entorno que no es producción");
+    // 	return [];
+    //   }
+  },
+};
+
+module.exports = nextConfig;
