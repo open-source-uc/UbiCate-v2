8bf8fcb080d582797495bf2174acc21584fe3435
mc-cari <mauriciocari2@gmail.com>
2024-02-21 23:29:29 -0300

    Remove main direct commit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,27 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+ 
+    if (process.env.NODE_ENV === "production") {
+      console.log("Ejecutando rewrites para producción");
+      return [
+        {
+          source: "/",
+          destination: "/form-geo",
+          permanent: true,
+        },
+        {
+          source: "/map",
+          destination: "/form-geo",
+          permanent: true,
+        },
+      ];
+    } else {
+      console.log("Ejecutando en un entorno que no es producción");
+      return [];
+    }
+  },
+};
 
 module.exports = nextConfig;
