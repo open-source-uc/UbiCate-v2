f60075eea8473554c75ebc9b8789d8058fbadb33
mc-cari <mauriciocari2@gmail.com>
2024-05-26 20:44:42 -0400

    fix(utils): refactor features to GeoJSON

--- utils/featuresToGeoJSON.tsx
+++ utils/featuresToGeoJSON.tsx
@@ -1,11 +1,6 @@
 export function featuresToGeoJSON(features: any): any {
-  return features
-    ? {
-        type: "FeatureCollection",
-        features: features,
-      }
-    : {
-        type: "FeatureCollection",
-        features: [],
-      };
+  return {
+    type: "FeatureCollection",
+    features: features ? features : [],
+  };
 }
