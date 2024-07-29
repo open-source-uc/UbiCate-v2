ea3224d1151a918574290df024fd75d58a0eef6c
mc-cari <mauriciocari2@gmail.com>
2024-07-29 17:26:32 -0400

    Remove image response

--- app/api/og-image/route.tsx
+++ app/api/og-image/route.tsx
@@ -1,28 +1,20 @@
-import { ImageResponse } from "next/og";
-
-import React from "react";
-
-export async function GET(request: any) {
-  const paramCampus: string | null = request.nextUrl.searchParams.get("campus");
-  const paramPlaceId: string | null = request.nextUrl.searchParams.get("place");
-
-  const imageUrl = `https://marketing4ecommerce.net/wp-content/uploads/2024/02/imagen-generada-con-midjourney-e1708680957351.jpg`;
-
-  const text = paramPlaceId ? paramPlaceId : paramCampus;
-  const textTruncated: string | null = text && text.length > 20 ? `${text.slice(0, 20)}...` : text;
-
-  return new ImageResponse(
-    (
-      <div tw="relative flex w-full h-full flex items-center justify-center">
-        <div tw="absolute flex inset-0">
-          <img src={imageUrl} alt="UbiCate UC - Mapa" width={1200} height={630} />
-          <div tw="absolute flex inset-0 bg-black bg-opacity-0" />
-        </div>
-        <div tw="flex flex-col h-full flex-col-reverse">
-          <div tw="flex h-1/3 items-center text-white text-8xl font-bold">{textTruncated}</div>
-        </div>
-      </div>
-    ),
+export async function GET() {
+  return new Response(
+    `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>UbiCate UC
+          </title>
+          <meta name="description" content="Buscador de salas ylugares de interés en campus de la Pontificia Universidad Católica de Chile." />
+          <meta name="author" content="Open Source UC" />
+          <meta name="keywords" content="Pontificia Universidad Católica de Chile, salas, campus, mapa, uc, ubicación, estudiantes, Open Source, san Joaquin, casa central, lo contador" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="og:title" content="UbiCate UC" />
+          <meta property="og:description" content="Buscador de salas ylugares de interés en campus de la Pontificia Universidad Católica de Chile." />
+          <meta property="og:image" content="" />
+          <meta property="og:image:width" content="1200" />
+        `,
   );
 }
 
