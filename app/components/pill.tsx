95e2e2437846cb86a8e634a581340229346aa7bb
MrBased <felipeaguilarwenig@uc.cl>
2024-08-07 19:52:46 -0400

    style(pills): readability

--- app/components/pill.tsx
+++ app/components/pill.tsx
@@ -24,15 +24,16 @@ function Pill({ title, iconPath }: PillProps) {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+  const mainStyle = "px-2 max-map-sm:flex max-map-sm:py-1 max-map-sm:justify-end";
+  const sectionStyle = `bg-white hover:bg-slate-200 border border-gray-300 pointer-events-auto cursor-pointer max-map-sm:w-9 max-map-sm:h-9 h-6 shadow-lg rounded-full flex justify-between max-map-sm:justify-center items-center font-bold ${inter.className}`;
+  const textStyle = `max-map-sm:invisible w-auto ${isSmallScreen ? "" : "px-3"}`;
+  const iconStyle = "pe-2 max-map-sm:pe-0 w-6 max-map-sm:w-5";
+
   return (
-    <main className="px-2 max-map-sm:flex max-map-sm:py-1 max-map-sm:justify-end">
-      <section
-        className={`bg-white hover:bg-slate-200 border border-gray-300 pointer-events-auto cursor-pointer max-map-sm:w-9 max-map-sm:h-9 h-6 shadow-lg rounded-full flex justify-between max-map-sm:justify-center items-center font-bold ${inter.className} `}
-      >
-        <p className={`max-map-sm:invisible w-auto ${isSmallScreen ? "" : "px-3"}`}>
-          {!isSmallScreen && <p>{title}</p>}
-        </p>
-        <Image className="pe-2 max-map-sm:pe-0 w-6 max-map-sm:w-5" src={iconPath} alt="logo" width={16} height={16} />
+    <main className={mainStyle}>
+      <section className={sectionStyle}>
+        {!isSmallScreen && <p className={textStyle}>{title}</p>}
+        <Image className={iconStyle} src={iconPath} alt="icon" width={16} height={16} />
       </section>
     </main>
   );
