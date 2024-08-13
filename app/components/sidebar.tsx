694c07d9049749e350b523aae4209615e558389c
mc-cari <mauriciocari2@gmail.com>
2024-08-12 23:52:03 -0400

    fix(sidebar): fix load error

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -15,54 +15,56 @@ export default function Sidebar() {
 
   return (
     <aside
-      className={`flex flex-col fixed inset-y-0 left-0 bg-white dark:bg-dark-1 dark:text-white text-white transform transition-transform duration-300 z-30 ${
+      className={`fixed inset-y-0 left-0 bg-white dark:bg-dark-1 dark:text-white text-white transform transition-transform duration-300 z-30 ${
         isOpen ? "translate-x-0 w-full sm:w-72" : "-translate-x-full"
       }`}
     >
-      <header className="h-12 bg-sky-600 flex items-center">
-        <div className="flex items-center p-1.5">
-          <button onClick={toggleSidebar} className="p-1.5 rounded-lg hover:bg-sky-700 ml-auto">
-            <ChevronLeft />
-          </button>
-        </div>
-      </header>
+      <div className="flex flex-col h-full">
+        <header className="h-12 bg-sky-600 flex items-center">
+          <div className="flex items-center p-1.5">
+            <button onClick={toggleSidebar} className="p-1.5 rounded-lg hover:bg-sky-700 ml-auto">
+              <ChevronLeft />
+            </button>
+          </div>
+        </header>
 
-      <nav className="flex flex-col grow p-4 shadow-xl">
-        <ul className="space-y-4 font-heading text-gray-700 dark:text-white">
-          <li>
-            <h3 className="text-lg font-bold mb-2">Campus</h3>
-            <ul className="space-y-2">
-              {campuses.features.map((campus) => (
-                <li key={campus.properties.campus}>
-                  <Link
-                    href={`/map?campus=${campus.properties.shortName}`}
-                    passHref
-                    className={`block px-4 py- rounded-lg  transition-all hover:text-sky-600 dark:hover:text-sky-300 ${
-                      searchParams.get("campus") === campus.properties.shortName
-                        ? "text-sky-600 dark:text-sky-300"
-                        : "text-gray-700 dark:text-white"
-                    }`}
-                    onClick={toggleSidebar}
-                  >
-                    {campus.properties.name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </li>
-          <hr className="border-t border-gray-300 dark:border-gray-700" />
-          <li>
-            <Link
-              onClick={toggleSidebar}
-              href={`/form-geo/${searchParams.get("campus") ? `?campus=${searchParams.get("campus")}` : ""}`}
-              className="flex items-center space-x-2 px-4 py-2 text-sky-600 dark:text-sky-300 rounded-full border border-sky-600 dark:border-sky-300 hover:bg-sky-300 dark:hover:bg-sky-700 transition-all"
-            >
-              <Plus />
-              <p>Agrega nueva ubicación</p>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+        <nav className="flex flex-col grow p-4 shadow-xl">
+          <ul className="space-y-4 font-heading text-gray-700 dark:text-white">
+            <li>
+              <h3 className="text-lg font-bold mb-2">Campus</h3>
+              <ul className="space-y-2">
+                {campuses.features.map((campus) => (
+                  <li key={campus.properties.campus}>
+                    <Link
+                      href={`/map?campus=${campus.properties.shortName}`}
+                      passHref
+                      className={`block px-4 py- rounded-lg  transition-all hover:text-sky-600 dark:hover:text-sky-300 ${
+                        searchParams.get("campus") === campus.properties.shortName
+                          ? "text-sky-600 dark:text-sky-300"
+                          : "text-gray-700 dark:text-white"
+                      }`}
+                      onClick={toggleSidebar}
+                    >
+                      {campus.properties.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </li>
+            <hr className="border-t border-gray-300 dark:border-gray-700" />
+            <li>
+              <Link
+                onClick={toggleSidebar}
+                href={`/form-geo/${searchParams.get("campus") ? `?campus=${searchParams.get("campus")}` : ""}`}
+                className="flex items-center space-x-2 px-4 py-2 text-sky-600 dark:text-sky-300 rounded-full border border-sky-600 dark:border-sky-300 hover:bg-sky-300 dark:hover:bg-sky-700 transition-all"
+              >
+                <Plus />
+                <p>Agrega nueva ubicación</p>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </aside>
   );
 }
