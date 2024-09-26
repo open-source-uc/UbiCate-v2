7588c2e0599190dd45d86145783fa4145ded0335
Utmite <rockimunoz94@gmail.com>
2024-09-26 18:40:54 -0300

    Sistema automatico de agregar ubicaciones al places.json 	- El sistema funciona con PR automaticas a una rama llamada bot-data-ABCD12

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -1,138 +1,135 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const GITHUB_TOKEN_USER = process.env.GITHUB_TOKEN_USER
-const BRANCH_NAME = "bot-data-ABCD12"
-const GITHUB_USER_EMAIL = process.env.GITHUB_USER_EMAIL
+const GITHUB_TOKEN_USER = process.env.GITHUB_TOKEN_USER;
+const BRANCH_NAME = "bot-data-ABCD12";
+const GITHUB_USER_EMAIL = process.env.GITHUB_USER_EMAIL;
 
 interface Place {
-    information: string;
-    floor: number;
-    latitude: number;
-    longitude: number;
-    name: string;
+  information: string;
+  floor: number;
+  latitude: number;
+  longitude: number;
+  name: string;
 }
 
 interface Places {
-    type: string;
-    features: Object[];
+  type: string;
+  features: Object[];
 }
 
 async function exist_rama_edit_data() {
-    try {
-        const response = await fetch(`https://api.github.com/repos/open-source-uc/UbiCate-v2/branches/${BRANCH_NAME}`, {
-            method: 'GET',
-            headers: {
-                'Authorization': `token ${GITHUB_TOKEN_USER}`,
-                'Accept': 'application/vnd.github.v3+json'
-            }
-        });
+  try {
+    const response = await fetch(`https://api.github.com/repos/open-source-uc/UbiCate-v2/branches/${BRANCH_NAME}`, {
+      method: "GET",
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN_USER}`,
+        Accept: "application/vnd.github.v3+json",
+      },
+    });
 
-        if (response.ok) {
-            return true;
-        } else {
-            return false;
-        }
-    } catch (error) {
-        console.error('Error en la solicitud:', error);
-        return false;
+    if (response.ok) {
+      return true;
+    } else {
+      return false;
     }
+  } catch (error) {
+    console.error("Error en la solicitud:", error);
+    return false;
+  }
 }
 
 async function create_branch() {
-    const baseBranchResponse = await fetch("https://api.github.com/repos/open-source-uc/UbiCate-v2/git/refs/heads/main", {
-        method: 'GET',
-        headers: {
-            'Authorization': `token ${GITHUB_TOKEN_USER}`,
-            'Accept': 'application/vnd.github.v3+json',
-        },
-    });
+  const baseBranchResponse = await fetch("https://api.github.com/repos/open-source-uc/UbiCate-v2/git/refs/heads/main", {
+    method: "GET",
+    headers: {
+      Authorization: `token ${GITHUB_TOKEN_USER}`,
+      Accept: "application/vnd.github.v3+json",
+    },
+  });
 
-    const baseBranchData = await baseBranchResponse.json();
-    const baseBranchSHA = baseBranchData.object.sha;
+  const baseBranchData = await baseBranchResponse.json();
+  const baseBranchSHA = baseBranchData.object.sha;
 
-    const branchData = {
-        ref: `refs/heads/${BRANCH_NAME}`,
-        sha: baseBranchSHA,
-    };
+  const branchData = {
+    ref: `refs/heads/${BRANCH_NAME}`,
+    sha: baseBranchSHA,
+  };
 
-    await fetch("https://api.github.com/repos/open-source-uc/UbiCate-v2/git/refs", {
-        method: 'POST',
-        headers: {
-            'Authorization': `token ${GITHUB_TOKEN_USER}`,
-            'Accept': 'application/vnd.github.v3+json',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(branchData),
-    });
-
+  await fetch("https://api.github.com/repos/open-source-uc/UbiCate-v2/git/refs", {
+    method: "POST",
+    headers: {
+      Authorization: `token ${GITHUB_TOKEN_USER}`,
+      Accept: "application/vnd.github.v3+json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(branchData),
+  });
 }
 
-
 export async function POST(request: NextRequest) {
-    request.json()
-        .then(async (body: Place) => {
+  request
+    .json()
+    .then(async (body: Place) => {
+      const nuevo_punto = {
+        type: "Feature",
+        properties: {
+          identifier: body.name,
+          name: body.name,
+          information: body.information,
+          categories: "",
+          campus: "SJ",
+          faculties: "",
+          floor: body.floor,
+          category: "classroom",
+        },
+        geometry: {
+          type: "Point",
+          coordinates: [body.longitude, body.latitude],
+        },
+      };
 
-            const nuevo_punto = {
-                "type": "Feature",
-                "properties": {
-                    "identifier": body.name,
-                    "name": body.name,
-                    "information": body.information,
-                    "categories": "",
-                    "campus": "SJ",
-                    "faculties": "",
-                    "floor": body.floor,
-                    "category": "classroom"
-                },
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [body.longitude, body.latitude]
-                }
-            }
+      const exist = await exist_rama_edit_data();
+      if (exist === false) {
+        console.log(`Se crea la rama ${BRANCH_NAME}`);
+        await create_branch();
+      }
+      const filePath = "data/places.json";
+      const url = `https://api.github.com/repos/open-source-uc/UbiCate-v2/contents/${filePath}?ref=${BRANCH_NAME}`;
 
-            const exist = await exist_rama_edit_data()
-            if (exist === false) {
-                console.log(`Se crea la rama ${BRANCH_NAME}`)
-                await create_branch()
-            }
-            const filePath = 'data/places.json'
-            const url = `https://api.github.com/repos/open-source-uc/UbiCate-v2/contents/${filePath}?ref=${BRANCH_NAME}`
+      const response = await fetch(url, {
+        headers: {
+          Authorization: `Bearer ${GITHUB_TOKEN_USER}`,
+          Accept: "application/vnd.github+json",
+        },
+      });
+      const file_data = await response.json();
 
-            const response = await fetch(url, {
-                headers: {
-                    Authorization: `Bearer ${GITHUB_TOKEN_USER}`,
-                    Accept: 'application/vnd.github+json',
-                },
-            });
-            const file_data = await response.json();
+      const file_sha = file_data.sha;
 
-            const file_sha = file_data.sha
+      const json = Buffer.from(file_data.content, "base64").toString();
+      const file_places: Places = JSON.parse(json);
+      file_places.features.push(nuevo_punto);
+      const r = await fetch(url, {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${GITHUB_TOKEN_USER}`,
+          Accept: "application/vnd.github+json",
+        },
+        body: JSON.stringify({
+          message: `Lugar nuevo ${nuevo_punto.properties.name}`,
+          committer: {
+            name: "BOT-PLACES",
+            email: GITHUB_USER_EMAIL,
+          },
+          content: Buffer.from(JSON.stringify(file_places)).toString("base64"),
+          sha: file_sha,
+          branch: BRANCH_NAME,
+        }),
+      });
 
-            const json = Buffer.from(file_data.content, "base64").toString();
-            const file_places: Places = JSON.parse(json);
-            file_places.features.push(nuevo_punto)
-            const r = await fetch(url, {
-                method: "PUT",
-                headers: {
-                    Authorization: `Bearer ${GITHUB_TOKEN_USER}`,
-                    Accept: 'application/vnd.github+json',
-                },
-                body: JSON.stringify({
-                    message: `Lugar nuevo ${nuevo_punto.properties.name}`,
-                    committer: {
-                        name: "BOT-PLACES",
-                        email: GITHUB_USER_EMAIL
-                    },
-                    content: Buffer.from(JSON.stringify(file_places)).toString("base64"),
-                    sha: file_sha,
-                    branch: BRANCH_NAME
-                })
-            })
-
-            return NextResponse.json({ message: 'GG', data: body });
-        })
-        .catch(error => {
-            return NextResponse.json({ error: 'Error al procesar el JSON' }, { status: 400 });
-        })
-
+      return NextResponse.json({ message: "GG", data: body });
+    })
+    .catch((error) => {
+      return NextResponse.json({ error: "Error al procesar el JSON" }, { status: 400 });
+    });
 }
