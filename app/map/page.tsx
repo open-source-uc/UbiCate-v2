60747e9260a99f871d27f77171ef4da050e00cea
Utmite <rockimunoz94@gmail.com>
2024-12-04 11:37:32 -0300

    technical debt

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -7,29 +7,26 @@ import PlacesJSON from "../../data/places.json";
 
 import MapComponent from "./map";
 
-type SearchParams = { campus?: string; place?: string; n?: string };
+type SearchParams = { campus?: string; place?: string; };
 
 export async function generateMetadata({ searchParams }: { searchParams: SearchParams }): Promise<Metadata> {
-  const placeName: string | undefined = searchParams?.n;
-
-  const params = new URLSearchParams();
-  if (placeName) params.append("n", placeName);
 
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "localhost:3000";
+  const paramPlaceId: string | undefined = searchParams?.place;
+  const paramPlace: Feature | null = (PlacesJSON.features.find((place) => place.properties.identifier === paramPlaceId) as Feature) ?? null;
+  const placeName = paramPlace?.properties?.name || null;
+
   return {
     title: placeName ? `UbíCate UC - ${placeName}` : "UbíCate UC - Mapa",
     openGraph: {
       images: [
         {
-          url: `${baseUrl}/api/og-image?n=${placeName}`,
+          url: new URL(`${baseUrl}/api/og-image?n=${placeName}`),
           width: 1200,
           height: 630,
           type: "image/png",
         },
       ],
-    },
-    alternates: {
-      canonical: `${baseUrl}/map${params.toString() ? `?${params.toString()}` : ""}`,
     },
   };
 }
