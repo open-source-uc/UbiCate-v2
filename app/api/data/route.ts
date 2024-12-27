002aff6f9475b39d08daf9cd4fefacf91bb6746f
Utmite <rockimunoz94@gmail.com>
2024-12-27 14:50:04 -0300

    needApproval

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -168,6 +168,7 @@ export async function PUT(request: NextRequest) {
         campus: body.campus,
         faculties: "",
         floors: [body.floor],
+        needApproval: true
       },
       geometry: {
         type: "Point",
