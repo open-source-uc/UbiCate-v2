6014930952a6f871482f6cebf7721f235f67dfac
Utmite <rockimunoz94@gmail.com>
2025-01-24 21:34:02 -0300

    (fix)lint:fix

--- app/map/map.tsx
+++ app/map/map.tsx
@@ -96,7 +96,7 @@ export default function MapComponent({
   // const [hover, setHover] = useState<Feature | null>(null);
 
   const [geocoderPlaces, setGeocoderPlaces] = useGeocoder(refMapNavbar, (place) => {
-    setPlace(null)
+    setPlace(null);
     if (place?.geometry.type === "Point") {
       mapRef.current?.getMap().flyTo({
         essential: true,
@@ -341,20 +341,20 @@ export default function MapComponent({
         ) : null} */}
         {geocoderPlaces
           ? geocoderPlaces
-            .filter((e) => e.geometry.type === "Point")
-            .map((place) => {
-              return (
-                <Marker
-                  key={place.properties.identifier}
-                  place={place as Place}
-                  onClick={() => {
-                    setTmpMark(null);
-                    onClickMark(place);
-                  }}
-                // onMouseEnter={setHover}
-                />
-              );
-            })
+              .filter((e) => e.geometry.type === "Point")
+              .map((place) => {
+                return (
+                  <Marker
+                    key={place.properties.identifier}
+                    place={place as Place}
+                    onClick={() => {
+                      setTmpMark(null);
+                      onClickMark(place);
+                    }}
+                    // onMouseEnter={setHover}
+                  />
+                );
+              })
           : null}
         {tmpMark && tmpMark.geometry.type === "Point" ? (
           <Marker
