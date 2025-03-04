bf42cf2d456c3b6592ee9255295ba433630b8807
IgnacioPalma <ignaciopalma.w@gmail.com>
2025-03-05 07:59:42 +0900

    Improved accesibility and style of Action button. Improved UI of mobile

--- app/components/locationButton.tsx
+++ app/components/locationButton.tsx
@@ -2,7 +2,7 @@ export default function LocationButton({ onClick }: { onClick?: () => void }) {
   return (
     <button
       onClick={() => onClick?.()}
-      className="p-3 rounded-full bg-brown-medium hover:bg-brown-dark text-white flex items-center justify-center w-14 h-14"
+      className="p-3 rounded-full bg-brown-medium border-1 border-brown-dark hover:bg-brown-light text-white flex items-center justify-center w-14 h-14 pointer-events-auto cursor-pointer"
     >
       <span className="material-symbols-outlined">my_location</span>
     </button>
