63681e2025873198eb734a4a16d1d988e00bdbe5
Utmite <rockimunoz94@gmail.com>
2025-03-05 12:24:51 -0300

    K

--- app/map/buttonsMaps.tsx
+++ app/map/buttonsMaps.tsx
@@ -1,124 +1,129 @@
-import { Feature, PointFeature } from "@/utils/types";
 import { useEffect, useState, useCallback } from "react";
-import Marker from "./marker";
+
+import type { MapRef } from "react-map-gl";
+
+import { Feature, PointFeature } from "@/utils/types";
+
 import * as Icons from "../components/icons/icons";
 import LocationButton from "../components/locationButton";
-import { MapRef } from "react-map-gl";
+
+import Marker from "./marker";
 
 export default function ButtonMaps({ mapRef }: { mapRef: React.RefObject<MapRef> }) {
-    const [userPosition, setUserPosition] = useState<Feature | null>(null);
-    const [isTracking, setIsTracking] = useState(false);
-    const [isFirstLocationFetch, setIsFirstLocationFetch] = useState(true);
+  const [userPosition, setUserPosition] = useState<Feature | null>(null);
+  const [isTracking, setIsTracking] = useState(false);
+  const [isFirstLocationFetch, setIsFirstLocationFetch] = useState(true);
 
-    const getUserLocation = useCallback(() => {
-        if (!navigator.geolocation) {
-            alert("La geolocalización no está soportada por este navegador.");
-            setIsTracking(false);
-            return;
-        }
+  const getUserLocation = useCallback(() => {
+    if (!navigator.geolocation) {
+      alert("La geolocalización no está soportada por este navegador.");
+      setIsTracking(false);
+      return;
+    }
 
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-                const { longitude, latitude } = position.coords;
-                const newPosition: PointFeature = {
-                    type: "Feature",
-                    properties: {
-                        identifier: "user_location_0001",
-                        name: "Mi Ubicación",
-                        information: "",
-                        categories: ["userLocation"],
-                        campus: "SJ",
-                        faculties: "",
-                        floors: [1],
-                        needApproval: false,
-                    },
-                    geometry: {
-                        type: "Point",
-                        coordinates: [longitude, latitude],
-                    },
-                };
-
-                setUserPosition(newPosition);
-
-                // If it's the first location fetch, automatically center the map
-                if (isFirstLocationFetch) {
-                    mapRef.current?.getMap().flyTo({
-                        center: newPosition.geometry.coordinates as [number, number],
-                        zoom: 17,
-                        duration: 400,
-                    });
-                    setIsFirstLocationFetch(false);
-                }
-            },
-            (positionError) => {
-                if (positionError.code === 1) {
-                    alert("Permiso de ubicación denegado. Por favor, habilite los permisos de ubicación en la configuración de su navegador.");
-                } else if (positionError.code === 2) {
-                    alert("La información de ubicación no está disponible.");
-                } else if (positionError.code === 3) {
-                    alert("La solicitud para obtener la ubicación ha expirado.");
-                } else {
-                    alert(`Error al obtener la ubicación: ${positionError.message}`);
-                }
-                setIsTracking(false);
-                setIsFirstLocationFetch(false);
-            },
-            {
-                enableHighAccuracy: true,
-                timeout: 5000,
-                maximumAge: 0,
-            }
-        );
-    }, [isFirstLocationFetch, mapRef]);
-
-    useEffect(() => {
-        let intervalId: NodeJS.Timeout;
-
-        if (isTracking) {
-            // Initial location fetch
-            getUserLocation();
-
-            // Periodic location updates
-            intervalId = setInterval(getUserLocation, 2000);
-        }
-
-        return () => {
-            if (intervalId) {
-                clearInterval(intervalId);
-            }
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { longitude, latitude } = position.coords;
+        const newPosition: PointFeature = {
+          type: "Feature",
+          properties: {
+            identifier: "user_location_0001",
+            name: "Mi Ubicación",
+            information: "",
+            categories: ["userLocation"],
+            campus: "SJ",
+            faculties: "",
+            floors: [1],
+            needApproval: false,
+          },
+          geometry: {
+            type: "Point",
+            coordinates: [longitude, latitude],
+          },
         };
-    }, [isTracking, getUserLocation]);
 
+        setUserPosition(newPosition);
 
-    const handleLocationButtonClick = () => {
-        if (userPosition === null) {
-            // If no position, start tracking
-            setIsTracking(true);
-            return;
-        }
-
-        // Fly to user's location
-        mapRef.current?.getMap().flyTo({
-            center: userPosition.geometry.coordinates as [number, number],
+        // If it's the first location fetch, automatically center the map
+        if (isFirstLocationFetch) {
+          mapRef.current?.getMap().flyTo({
+            center: newPosition.geometry.coordinates as [number, number],
             zoom: 17,
             duration: 400,
-        });
-    };
-
-    return (
-        <>
-            {userPosition && (
-                <Marker
-                    key={userPosition.properties.identifier}
-                    place={userPosition as PointFeature}
-                    onClick={() => null}
-                    icon={<Icons.UserLocation />}
-                />
-            )}
-
-            <div className="fixed z-40 bottom-17 desktop:bottom-0 right-0 p-2">
-                <LocationButton onClick={handleLocationButtonClick} />
-            </div>
-        </>
+          });
+          setIsFirstLocationFetch(false);
+        }
+      },
+      (positionError) => {
+        if (positionError.code === 1) {
+          alert(
+            "Permiso de ubicación denegado. Por favor, habilite los permisos de ubicación en la configuración de su navegador.",
+          );
+        } else if (positionError.code === 2) {
+          alert("La información de ubicación no está disponible.");
+        } else if (positionError.code === 3) {
+          alert("La solicitud para obtener la ubicación ha expirado.");
+        } else {
+          alert(`Error al obtener la ubicación: ${positionError.message}`);
+        }
+        setIsTracking(false);
+        setIsFirstLocationFetch(false);
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      },
     );
+  }, [isFirstLocationFetch, mapRef]);
+
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout;
+
+    if (isTracking) {
+      // Initial location fetch
+      getUserLocation();
+
+      // Periodic location updates
+      intervalId = setInterval(getUserLocation, 2000);
+    }
+
+    return () => {
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
+    };
+  }, [isTracking, getUserLocation]);
+
+  const handleLocationButtonClick = () => {
+    if (userPosition === null) {
+      // If no position, start tracking
+      setIsTracking(true);
+      return;
+    }
+
+    // Fly to user's location
+    mapRef.current?.getMap().flyTo({
+      center: userPosition.geometry.coordinates as [number, number],
+      zoom: 17,
+      duration: 400,
+    });
+  };
+
+  return (
+    <>
+      {userPosition ? (
+        <Marker
+          key={userPosition.properties.identifier}
+          place={userPosition as PointFeature}
+          onClick={() => null}
+          icon={<Icons.UserLocation />}
+        />
+      ) : null}
+
+      <div className="fixed z-40 bottom-17 desktop:bottom-0 right-0 p-2">
+        <LocationButton onClick={handleLocationButtonClick} />
+      </div>
+    </>
+  );
 }
--- app/map/map.tsx
+++ app/map/map.tsx
@@ -32,9 +32,9 @@ import * as Icons from "../components/icons/icons";
 import MarkerIcon from "../components/icons/markerIcon";
 import { useSidebar } from "../context/sidebarCtx";
 
+import ButtonMaps from "./buttonsMaps";
 import { placesTextLayer, campusBorderLayer, sectionAreaLayer, sectionStrokeLayer } from "./layers";
 import Marker from "./marker";
-import ButtonMaps from "./buttonsMaps";
 
 interface InitialViewState extends Partial<ViewState> {
   bounds?: LngLatBoundsLike;
@@ -338,13 +338,13 @@ export default function MapComponent({
       metaDescription.setAttribute(
         "content",
         selectedPlace
-          ? `Nombre: ${selectedPlace.properties.name}; Categoria: ${siglas.get(selectedPlace.properties.categories[0]) ?? "Sala"
-          }; Piso: ${selectedPlace.properties.floors?.[0] ?? "N/A"}`
+          ? `Nombre: ${selectedPlace.properties.name}; Categoria: ${
+              siglas.get(selectedPlace.properties.categories[0]) ?? "Sala"
+            }; Piso: ${selectedPlace.properties.floors?.[0] ?? "N/A"}`
           : "Encuentra fácilmente salas de clases, baños, bibliotecas y puntos de comida en los campus de la Pontificia Universidad Católica (PUC). Nuestra herramienta interactiva te ayuda a navegar de manera rápida y eficiente. ¡Explora y descubre todo lo que necesitas al alcance de tu mano! Busca Salas UC",
       );
     }
   }, [selectedPlace]);
-
 
   return (
     <>
@@ -401,7 +401,7 @@ export default function MapComponent({
             {hover.properties.name}
           </Popup>
         ) : null} */}
-        <ButtonMaps mapRef={mapRef}></ButtonMaps>
+        <ButtonMaps mapRef={mapRef} />
         {points.map((place) => {
           const primaryCategory = place.properties.categories[0] as Category;
           return (
