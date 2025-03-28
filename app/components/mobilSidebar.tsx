67c9acc7c420584a51cc219f72ff073eb5573138
Utmite <rockimunoz94@gmail.com>
2025-03-28 11:25:15 -0300

    k

--- app/components/mobilSidebar.tsx
+++ app/components/mobilSidebar.tsx
@@ -13,7 +13,6 @@ import MenuInformation from "../map/menuInformation";
 
 import CampusList from "./campusList";
 import FooterOptionsSidebar from "./footerOptionsSidebar";
-import Pill from "./pill";
 import PillFilter from "./pillFilterBar";
 
 export default function MobileSidebar() {
@@ -198,7 +197,7 @@ export default function MobileSidebar() {
       {/* Search Container */}
       <section className="fixed top-0 right-0 w-full justify-center z-50 py-2 px-4 flex flex-col">
         <div ref={refSearchContainer} className="w-full" />
-        <div className="w-full h-16 py-2 items-center justify-center">
+        {/* <div className="w-full h-16 py-2 items-center justify-center">
           <Pill
             title={"¡Ubícate en la UC y gana premios! 🎊"}
             className="w-full truncate rounded-xl flex items-center px-2 py-1.5 border-1 border-brown-medium desktop:border-transparent"
@@ -227,7 +226,7 @@ export default function MobileSidebar() {
             active={false}
             noActivateClassName="bg-brown-dark desktop:bg-brown-medium text-white-ubi"
           />
-        </div>
+        </div> */}
       </section>
 
       {/* Main Sidebar */}
