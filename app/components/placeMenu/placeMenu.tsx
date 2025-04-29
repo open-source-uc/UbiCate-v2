1c99d2a184147a90fffc9eb4245ee660440fb85e
Utmite <rockimunoz94@gmail.com>
2025-04-29 19:01:26 -0400

    k

--- app/components/placeMenu/placeMenu.tsx
+++ app/components/placeMenu/placeMenu.tsx
@@ -4,8 +4,6 @@ import PlaceInformation from "./placeInformation";
 import { use, useState } from "react";
 import { pinsContext } from "@/app/context/pinsCtx";
 import PlaceForm from "../forms/PlaceForm";
-import { useSidebar } from "@/app/context/sidebarCtx";
-import { on } from "events";
 
 export default function placeMenu({
   place,
@@ -22,8 +20,67 @@ export default function placeMenu({
 }) {
   const [mode, setMode] = useState<"information" | "create" | "edit">("information");
   const { clearPins, addPin } = use(pinsContext)
-  const { selectedPlace } = useSidebar();
   const [editPlace, setEditPlace] = useState<Feature | null>(null);
+
+  const approvePlace = () => {
+    const confirmacion = confirm("Estas seguro de APROBAR el lugar") ?? false;
+    if (!confirmacion) return;
+
+    fetch("api/ubicate", {
+      method: "PATCH",
+      headers: {
+        "ubicate-token": sessionStorage.getItem("ubicateToken") ?? "",
+      },
+      body: JSON.stringify({
+        identifier: place?.properties.identifier,
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          res
+            .json()
+            .then((data) => {
+              alert("Hubo un error: " + data.message);
+            })
+            .catch(() => {
+              alert("Hubo un error: " + res.status);
+            });
+          return;
+        }
+        alert("Se aprobo");
+        document.location.reload();
+      })
+      .catch(() => {
+        alert("Hubo un error");
+      });
+  };
+
+  const deletePlace = () => {
+    const confirmacion =
+      confirm("Estas seguro de BORRAR el lugar " + (sessionStorage.getItem("ubicateToken") ?? "")) ?? false;
+    if (!confirmacion) return;
+
+    fetch("api/ubicate", {
+      method: "DELETE",
+      headers: {
+        "ubicate-token": sessionStorage.getItem("ubicateToken") ?? "",
+      },
+      body: JSON.stringify({
+        identifier: place?.properties.identifier,
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          alert("Hubo un error");
+          return;
+        }
+        alert("Se borro");
+        document.location.reload();
+      })
+      .catch(() => {
+        alert("Hubo un error");
+      });
+  };
 
   return (
     <div className="flex flex-col gap-2 py-2">
@@ -38,31 +95,22 @@ export default function placeMenu({
           onEdit={() => {
             clearPins();
             setMode(() => "edit")
-            setEditPlace(() => structuredClone(selectedPlace));
-            if (selectedPlace?.geometry.type === "Point") {
-              addPin(selectedPlace.geometry.coordinates[0], selectedPlace.geometry.coordinates[1])
+            setEditPlace(() => structuredClone(place));
+            if (place?.geometry.type === "Point") {
+              addPin(place.geometry.coordinates[0], place.geometry.coordinates[1])
             }
-            if (selectedPlace?.geometry.type === "Polygon") {
-              selectedPlace.geometry.coordinates[0].slice(0, -1).forEach((coord) => {
+            if (place?.geometry.type === "Polygon") {
+              place.geometry.coordinates[0].slice(0, -1).forEach((coord) => {
                 addPin(coord[0], coord[1]);
               });
             }
             onOpenEdit?.();
           }}
           onReject={() => {
-            const confirmacion = confirm("Estas seguro de RECHAZAR el lugar") ?? false;
-            if (!confirmacion) return;
-
-            fetch("api/ubicate", {
-              method: "PATCH",
-              headers: {
-                "ubicate-token": sessionStorage.getItem("ubicateToken") ?? "",
-              },
-              body: JSON.stringify({
-                identifier: place.properties.identifier,
-                status: "REJECTED",
-              }),
-            })
+            deletePlace()
+          }}
+          onApprove={() => {
+            approvePlace()
           }}
         />
       )}
