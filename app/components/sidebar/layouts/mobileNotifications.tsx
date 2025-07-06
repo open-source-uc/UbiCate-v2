f9a8d7f9e18c98c312fbd8fd374e3a61bb181fa6
IgnacioPalma <ignaciopalma.w@gmail.com>
2025-07-06 20:25:23 +0000

    Fixed bug on the notifications

--- app/components/sidebar/layouts/mobileNotifications.tsx
+++ app/components/sidebar/layouts/mobileNotifications.tsx
@@ -10,7 +10,7 @@ export default function TopMobileSidebar({
   const { component } = use(NotificationContext);
 
   return (
-    <section className="fixed top-0 left-0 w-full z-50 py-2 px-4 flex flex-col md:flex-row lg:items-start lg:justify-start gap-2 pointer-events-none bg-background text-foreground">
+    <section className="fixed top-0 left-0 w-full z-50 py-2 px-4 flex flex-col md:flex-row lg:items-start lg:justify-start gap-2 pointer-events-none">
       <div className="w-full max-w-[450px] mr-auto flex flex-col gap-2">
         <div ref={refSearchContainer} className="w-full" />
         {component ? component : null}
