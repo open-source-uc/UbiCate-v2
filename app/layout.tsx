e92721a2d80ed8407eb5563bdd5a03a02b2b80d0
Utmite <rockimunoz94@gmail.com>
2025-07-21 11:56:39 -0400

    rollback(nextjs version metatags error)

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,8 @@ import type { Viewport } from "next";
 
 import SWRegister from "./components/SWRegister";
 
-
 export const metadata: Metadata = {
-  metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL|| "http://localhost:3000/"),
+  metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000/"),
   title: "Ubicate UC",
   description:
     "Encuentra fácilmente salas de clases, baños, bibliotecas y puntos de comida en los campus de la Pontificia Universidad Católica (PUC). Nuestra herramienta interactiva te ayuda a navegar de manera rápida y eficiente, optimizando tu tiempo y mejorando tu experiencia en la universidad. ¡Explora y descubre todo lo que necesitas al alcance de tu mano! Busca Salas UC",
