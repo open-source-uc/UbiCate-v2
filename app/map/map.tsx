415984f7e595cd29cbd923baed0856dda0b38f27
Utmite <rockimunoz94@gmail.com>
2025-07-22 20:00:37 -0400

    rollback

--- app/map/map.tsx
+++ app/map/map.tsx
@@ -313,12 +313,13 @@ export default function MapComponent({
           });
         }}
         transformRequest={(url, type) => {
-          if (type === "Tile" || type === "Glyphs") {
-            if (process.env.NEXT_PUBLIC_IS_SELF_HOST === "TRUE") {
-              return { url: window.location.origin + url };
-            } else {
-              return { url: `https://ubicate.osuc.dev${url}` };
-            }
+          if (type === "Tile") {
+            const baseUrl = window.location.origin;
+            return { url: baseUrl + url };
+          }
+          if (type === "Glyphs") {
+            const baseUrl = window.location.origin;
+            return { url: baseUrl + url };
           }
           return { url };
         }}
