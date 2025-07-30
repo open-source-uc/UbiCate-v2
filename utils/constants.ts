f8cf05a743e950c4205d2c016bedf97159a3480e
Benja-Ch <benjamin.cheuquianhuenul@gmail.com>
2025-07-30 02:36:06 -0400

    nombre de piso 1, sin añadido

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,9 +1,10 @@
 // ID de los lugares fijos que siempre deben mostrarse
-export const ALWAYS_VISIBLE_IDS = new Set(["OTHER-34012515", "OTHER-32467011", "OTHER-26347017"]);
+export const ALWAYS_VISIBLE_IDS = new Set(["OTHER-34012515", "OTHER-32467011", "OTHER-26347017", "OSUC"]);
 
 // Íconos asociados a esos lugares
 export const PLACE_ICONS: Record<string, { icon: string; size?: string }> = {
   "OTHER-34012515": { icon: "Baticristo", size: "w-8 h-8" },
   "OTHER-32467011": { icon: "Meones", size: "w-8 h-8" },
   "OTHER-26347017": { icon: "TemploSanJoaquin", size: "w-8 h-8" },
+  OSUC: { icon: "OSUC", size: "w-8 h-8" },
 };
