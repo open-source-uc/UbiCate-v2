4f920c5c0ac6936562b44a9d111ec246a5f34dee
Utmite <rockimunoz94@gmail.com>
2025-08-03 12:35:44 -0400

    hotfix(tanstack)

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,8 +1,7 @@
 "use client";
+import React, { useMemo } from "react";
 
-import React from "react";
-
-import { isServer, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { DirectionsProvider } from "./context/directionsCtx";
 import { NotificationProvider } from "./context/notificationCtx";
@@ -21,37 +20,26 @@ function makeQueryClient() {
   });
 }
 
-let browserQueryClient: QueryClient | undefined = undefined;
-
-function getQueryClient() {
-  if (isServer) {
-    return makeQueryClient();
-  } else {
-    if (!browserQueryClient) browserQueryClient = makeQueryClient();
-    return browserQueryClient;
-  }
-}
-
 interface ProvidersProps {
   children: React.ReactNode;
 }
 
 export default function Providers({ children }: ProvidersProps) {
-  const queryClient = getQueryClient();
+  const queryClient = useMemo(() => makeQueryClient(), []);
 
   return (
     <QueryClientProvider client={queryClient}>
-      <UbicationProvider>
-        <SidebarProvider>
-          <DirectionsProvider>
-            <PinsProvider>
-              <NotificationProvider>
-                <ThemeProvider>{children}</ThemeProvider>
-              </NotificationProvider>
-            </PinsProvider>
-          </DirectionsProvider>
-        </SidebarProvider>
-      </UbicationProvider>
+      <ThemeProvider>
+        <UbicationProvider>
+          <SidebarProvider>
+            <DirectionsProvider>
+              <PinsProvider>
+                <NotificationProvider>{children}</NotificationProvider>
+              </PinsProvider>
+            </DirectionsProvider>
+          </SidebarProvider>
+        </UbicationProvider>
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
