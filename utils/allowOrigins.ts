5f71e9c36887cfbf85fc54c3a5ac2e6dddbd1516
Utmite <rockimunoz94@gmail.com>
2025-08-03 13:45:46 -0400

    feat(origins)

--- utils/allowOrigins.ts
+++ utils/allowOrigins.ts
@@ -9,8 +9,11 @@ export function getAllowedOrigin(origin: string | null): string | null {
       return origin;
     }
 
-    if (hostname === 'osuc.dev' || hostname.endsWith('.osuc.dev') ||
-        hostname === 'uc.cl' || hostname.endsWith('.uc.cl')) {
+    if (hostname === 'osuc.dev' 
+      || hostname.endsWith('.osuc.dev') 
+      || hostname === 'uc.cl' 
+        || hostname.endsWith('.uc.cl') ||
+        hostname === 'ubicate-v2.pages.dev' || hostname.endsWith('.ubicate-v2.pages.dev')) {
       return origin;
     }
   } catch (error) {
