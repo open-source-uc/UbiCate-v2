03774ccd0ff80ebf9a422fb57affe03a108b3789
Utmite <rockimunoz94@gmail.com>
2025-08-03 22:51:01 -0400

    ll

--- utils/categoryToColors.ts
+++ utils/categoryToColors.ts
@@ -1,22 +1,26 @@
+import { CATEGORIES } from "./types";
+
 const categoryToColorMap = new Map<string, string>([
-  ["faculty", "bg-chart-1"],
-  ["studyroom", "bg-chart-2"],
-  ["auditorium", "bg-chart-3"],
-  ["biblioteca", "bg-chart-4"],
-  ["bath", "bg-chart-5"],
-  ["food_lunch", "bg-chart-6"],
-  ["water", "bg-chart-7"],
-  ["sports_place", "bg-chart-8"],
-  ["crisol", "bg-chart-9"],
-  ["parking", "bg-chart-10"],
-  ["computers", "bg-chart-9"],
-  ["Facultad", "bg-chart-6"],
-  ["library", "bg-chart-4"],
-  ["userLocation", "bg-primary"],
-  ["customMark", "bg-chart-4"],
-  ["photocopy", "bg-chart-3"],
+  [CATEGORIES.FACULTY, "bg-chart-1"],
+  [CATEGORIES.STUDYROOM, "bg-chart-2"],
+  [CATEGORIES.AUDITORIUM, "bg-chart-3"],
+  [CATEGORIES.BATH, "bg-chart-5"],
+  [CATEGORIES.FOOD_LUNCH, "bg-chart-6"],
+  [CATEGORIES.WATER, "bg-chart-7"],
+  [CATEGORIES.SPORTS_PLACE, "bg-chart-8"],
+  // [CATEGORIES.CRISOL, "bg-chart-9"],
+  [CATEGORIES.PARKING, "bg-chart-10"],
+  [CATEGORIES.COMPUTERS, "bg-chart-9"],
+  [CATEGORIES.FACULTY, "bg-chart-6"],
+  [CATEGORIES.LIBRARY, "bg-chart-4"],
+  [CATEGORIES.USER_LOCATION, "bg-primary"],
+  [CATEGORIES.CUSTOM_MARK, "bg-chart-4"],
+  [CATEGORIES.PHOTOCOPY, "bg-chart-1"],
+  [CATEGORIES.FINANCIAL, "bg-chart-3"],
+  [CATEGORIES.SHOP, "bg-chart-2"],
+  [CATEGORIES.PARK_BICYCLE, "bg-chart-5"],
 ]);
 
 export const getCategoryColor = (category: string): string => {
-  return categoryToColorMap.get(category) ?? "bg-chart-0"; // Default color if category not found
+  return categoryToColorMap.get(category) ?? "bg-chart-0";
 };
