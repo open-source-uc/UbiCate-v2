d15c229784270926c97692b526c52252b7b2fbfb
Utmite <rockimunoz94@gmail.com>
2025-08-06 11:05:56 -0400

    lint

--- app/sw.ts
+++ app/sw.ts
@@ -9,9 +9,7 @@ declare global {
 declare const self: ServiceWorkerGlobalScope;
 
 const serwist = new Serwist({
-  precacheEntries: [
-    ...(self.__SW_MANIFEST || [])
-  ],
+  precacheEntries: [...(self.__SW_MANIFEST || [])],
   skipWaiting: true,
   clientsClaim: true,
   navigationPreload: true,
