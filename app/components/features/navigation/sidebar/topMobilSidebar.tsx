a466dcd7c75bcbe7334060bb3cd319888509b41d
IgnacioPalma <ignaciopalma.w@gmail.com>
2025-10-02 20:17:34 -0300

    fix lint

--- app/components/features/navigation/sidebar/topMobilSidebar.tsx
+++ app/components/features/navigation/sidebar/topMobilSidebar.tsx
@@ -8,12 +8,12 @@ export default function TopMobileSidebar() {
   const { component } = use(NotificationContext);
 
   return (
-    <section 
+    <section
       className="fixed top-0 left-0 w-full z-50 py-2 px-4 flex flex-col md:flex-row lg:items-start lg:justify-start gap-2 pointer-events-auto bg-theme-background text-theme-color"
       style={{
-        paddingTop: 'max(0.5rem, env(safe-area-inset-top))',
-        paddingLeft: 'env(safe-area-inset-left)',
-        paddingRight: 'env(safe-area-inset-right)',
+        paddingTop: "max(0.5rem, env(safe-area-inset-top))",
+        paddingLeft: "env(safe-area-inset-left)",
+        paddingRight: "env(safe-area-inset-right)",
       }}
     >
       <div className="w-full max-w-[450px] mr-auto flex flex-col gap-2">
