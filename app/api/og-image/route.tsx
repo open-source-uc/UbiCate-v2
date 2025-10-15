d502ed9e152480d3e06754581f49423d67801f8c
IgnacioPalma <ignaciopalma.w@gmail.com>
2025-10-15 17:40:35 -0300

    feat: update OpenGraph image generation with new font and improved text handling

--- app/api/og-image/route.tsx
+++ app/api/og-image/route.tsx
@@ -5,10 +5,10 @@ function Template({ text, url }: { text: string; url: string }) {
   return (
     <div tw="relative flex w-full h-full flex items-start justify-start">
       <div tw="absolute flex inset-0">
-        <img src={url} alt="Ubicate UC - Mapa" width={1200} height={630} />
+        <img src={url} alt="Ubicate" width={1200} height={630} />
         <div tw="absolute flex inset-0 bg-black bg-opacity-0" />
       </div>
-      <div tw="flex flex-col h-full pl-16 pt-16 pr-16">
+      <div tw="flex flex-col h-ful pl-16 pt-16 pr-16">
         <div tw="flex text-white text-8xl font-black leading-tight max-w-full" style={{ wordWrap: "break-word" }}>
           {text}
         </div>
@@ -22,13 +22,23 @@ export async function GET(request: NextRequest) {
   const url = new URL(request.nextUrl.href);
   const baseUrl = `${url.origin.toString()}`;
 
-  const text = placeName === "" ? "Ubicate UC · Mapa" : placeName;
-  const textTruncated: string = text && text.length > 40 ? `${text.slice(0, 40)}...` : text;
+  const font = await fetch(new URL("/public/fonts/InstrumentSans-SemiBold.ttf", import.meta.url));
+  const fontData = await font.arrayBuffer();
+
+  const text = placeName === "" ? "Find Everything in your Campus" : placeName;
+  const textTruncated: string = text && text.length > 60 ? `${text.slice(0, 60)}...` : text;
 
   return new ImageResponse(<Template text={textTruncated} url={baseUrl + "/opengraph-image.png"} />, {
     width: 1200,
     height: 630,
     status: 200,
+    fonts: [
+      {
+        name: "Instrument Sans",
+        data: fontData,
+        style: "normal",
+      },
+    ],
   });
 }
 
