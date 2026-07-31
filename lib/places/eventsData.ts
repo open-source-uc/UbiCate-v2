42a3f3c8f93281a23ebba3e58f0f04419e1a8937
mflor <mflor@uc.cl>
2026-07-31 14:10:34 -0400

    Build fix

--- lib/places/eventsData.ts
+++ lib/places/eventsData.ts
@@ -1,7 +1,7 @@
 import rawEvents from "@/data/eventPlaces.json";
 import { EventFeature, EventJSONFeatures } from "@/lib/types";
 
-const EventsJSON: EventJSONFeatures = rawEvents as EventJSONFeatures;
+const EventsJSON: EventJSONFeatures = rawEvents as unknown as EventJSONFeatures;
 
 export const allEvents: EventFeature[] = EventsJSON.features.filter(
   (f): f is EventFeature => "startDate" in f.properties,
