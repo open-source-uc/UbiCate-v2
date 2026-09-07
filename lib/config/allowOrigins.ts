0c0bd1e43a3fb6a47268e0c423585d4458be5767
PabloCruzval <p.cruzat.valenzuela@gmail.com>
2026-09-07 17:56:23 -0300

    fix: lint

--- lib/config/allowOrigins.ts
+++ lib/config/allowOrigins.ts
@@ -11,7 +11,12 @@ export function getAllowedOrigin(origin: string | null): string | null {
     }
 
     // dominios de producción
-    const allowedHostnames = ["osuc.dev", "uc.cl", "ubicate-v2.pages.dev", "ca-ubicate-dtfd-prod-central-001.bluebeach-090562db.centralus.azurecontainerapps.io"];
+    const allowedHostnames = [
+      "osuc.dev",
+      "uc.cl",
+      "ubicate-v2.pages.dev",
+      "ca-ubicate-dtfd-prod-central-001.bluebeach-090562db.centralus.azurecontainerapps.io",
+    ];
 
     if (allowedHostnames.includes(hostname) || allowedHostnames.some((h) => hostname.endsWith("." + h))) {
       return origin;
